c2b82b65a34aa6f1583fb6df5a71714940664529
Denis <Dnik1993@yandex.ru>
2023-03-09 20:25:15 +0300

    Add Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу которая на вход принимает два чила и выдаёт которое из них максимальное и минимальное
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+    Console.WriteLine($"max {a}\nmin {b}");
+else if (a<b)
+    Console.WriteLine($"max {b}\nmin {a}");
+else
+    Console.WriteLine("они равны");
