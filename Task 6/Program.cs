a8a3102898ac69e622902e5e368c10b48502f98d
Denis <Dnik1993@yandex.ru>
2023-03-09 20:27:59 +0300

    Add Task6

---
+++ Task 6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимает чило и выдает четное или нет
+Console.Clear();
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n%2==0)
+    Console.WriteLine("Чётное");
+else
+    Console.WriteLine("Нечётное");
+
