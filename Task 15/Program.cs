d6d3b00b7cd1c84836512617899ad75b9c645cd8
Denis <Dnik1993@yandex.ru>
2023-03-13 18:54:04 +0300

    Task 15

---
+++ Task 15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите день недели от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>=1 && n<=7){
+    if (n>5 && n<=7)
+        Console.WriteLine("Выходной");
+    else
+        Console.WriteLine("Рабочий день");
+}
+else   
+    Console.WriteLine("Это не день недели");
