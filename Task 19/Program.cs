14ef9d8127c2f44ebbba9db64df75f07d3283111
Denis <Dnik1993@yandex.ru>
2023-03-15 13:52:49 +0300

    Task 19

---
+++ Task 19/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите введите пятизначное число :");
+string s = Console.ReadLine()!;
+
+
+if(s[0]==s[4]){
+    if(s[1]==s[3])
+        Console.WriteLine("число полиндром");
+}
+else
+Console.WriteLine("Число не полиндром");
