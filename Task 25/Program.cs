4b7396ba7ccedf6e67259be8d4ca0b2e3e305ac1
Denis <Dnik1993@yandex.ru>
2023-03-22 11:59:53 +0300

    Task 25

--- Task 25/Program.cs
+++ Task 25/Program.cs
@@ -1,2 +1,8 @@
 ﻿Console.Clear();
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+for ( int i = 1; i <= b ; i++);
+    a *= a;
+Console.WriteLine(a);
