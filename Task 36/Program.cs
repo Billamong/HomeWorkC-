0622ee1bafa9e99318ca73915f92114d9742ea55
Denis <Dnik1993@yandex.ru>
2023-03-22 14:12:15 +0300

    Task 36

---
+++ Task 36/Program.cs
@@ -0,0 +1,21 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 100);
+}
+
+int sumNechetElement (int[] array){
+    int sum=0;
+    for (int i = 1; i < array.Length; i=i+2){
+        sum +=array[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine(sumNechetElement(array));
