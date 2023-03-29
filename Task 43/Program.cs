e9e71efd9bdd0ebcb1a1188e178d7dd1e374a326
Denis <Dnik1993@yandex.ru>
2023-03-30 00:51:27 +0300

    Task 43

---
+++ Task 43/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая найдет точку пересечения двух правых,
+// заданных уравнениями y = k1 * x +b1, y = k2 * x + b2; 
+// значения k1, b1, k2, b2 задаются пользователем.
+
+Console.Clear();
+Console.WriteLine(" Введите b1:");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите k1:");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите b2:");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите k2:");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = Convert.ToDouble((b2-b1) / (k1-k2));
+double y = Convert.ToDouble(k1 * x + b1);
+Console.WriteLine($"x={x}, y={y}");
