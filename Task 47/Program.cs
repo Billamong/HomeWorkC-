2180d5554108f1d64b2a7af747f03a116065df0d
Denis <Dnik1993@yandex.ru>
2023-03-31 11:15:19 +0300

    Task 47

---
+++ Task 47/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (0 - 20) + 10, 2);
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+double[,] matrixx = new double[3, 4];
+InputMatrix(matrixx);
+PrintMatrix(matrixx);
