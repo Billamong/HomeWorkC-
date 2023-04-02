f1c9c502fb8e09b7f584acae32ef73f6ea7cd86f
Denis <Dnik1993@yandex.ru>
2023-04-02 16:32:37 +0300

    Task 56

---
+++ Task 56/Program.cs
@@ -0,0 +1,48 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11);
+        }
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int MinStroka(int[,] matrix)
+{
+    int sum = 0, minsum = 0, count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        if (minsum > sum || i == 0)
+        {
+            minsum = sum;
+            count = i;
+        }
+        sum = 0;
+    }
+    return count + 1;
+}
+
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrixx = new int[size[0], size[1]];
+InputMatrix(matrixx);
+PrintMatrix(matrixx);
+Console.WriteLine($"Строка с минимальной суммой : {MinStroka(matrixx)}");
