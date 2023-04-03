c51bb212f4acba4475ac191093de7a1fe575af92
Denis <Dnik1993@yandex.ru>
2023-04-03 09:43:30 +0300

    Task 58

---
+++ Task 58/Program.cs
@@ -0,0 +1,51 @@
+﻿
+void inputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ProizveDenie(int[,] matrix1, int[,] matrix2, int[,] matrix3){
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix1.GetLength(1); j++)
+        {
+            for(int k=0; k < matrix3.GetLength(0); k++){
+                matrix3[i,j] += matrix1[i,k]*matrix2[k,j];
+            }
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int coord = Convert.ToInt32(Console.ReadLine());
+int[,] matrixA = new int[coord, coord];
+int[,] matrixB = new int[coord, coord];
+int[,] matrixC = new int[coord, coord];
+inputMatrix(matrixA);
+inputMatrix(matrixB);
+PrintMatrix(matrixA);
+Console.WriteLine();
+PrintMatrix(matrixB);
+Console.WriteLine();
+ProizveDenie(matrixA, matrixB, matrixC);
+PrintMatrix(matrixC);
