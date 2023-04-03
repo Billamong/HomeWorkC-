2a80d3871367ac8f561102737410d22b838a84ef
Denis <Dnik1993@yandex.ru>
2023-04-03 10:31:25 +0300

    Task 60

---
+++ Task 60/Program.cs
@@ -0,0 +1,37 @@
+﻿
+void inputMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k =0; k < matrix.GetLength(2); k++){
+                matrix[i, j, k] = new Random().Next(0, 100);
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k})\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int coord = Convert.ToInt32(Console.ReadLine());
+int[,,] matrix = new int[coord, coord, coord];
+inputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
