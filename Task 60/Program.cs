13b61a6c77006f55aa7808cbe69bb604608eedd8
Denis <Dnik1993@yandex.ru>
2023-04-04 22:15:40 +0300

    Task 60

--- Task 60/Program.cs
+++ Task 60/Program.cs
@@ -1,17 +1,38 @@
 ﻿
-void inputMatrix(int[,,] matrix)
+
+void inputMatrix(int[,,] matrix, int[] array)
 {
+    int r=0;
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            for (int k =0; k < matrix.GetLength(2); k++){
-                matrix[i, j, k] = new Random().Next(0, 100);
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = new Random().Next(10, 100);
+                array[r] = matrix[i, j, k];
+                r++;
             }
         }
-        Console.WriteLine();
     }
 }
+void SwapFirstLastString(int[,,] matrix, int[] array)
+{
+    int t = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (array[t] == matrix[i, j, k])
+                    inputMatrix(matrix, array);
+                t++;
+            }
+        }
+    }
+}
+
 
 void PrintMatrix(int[,,] matrix)
 {
@@ -32,6 +53,9 @@ Console.Clear();
 Console.Write("Введите размеры матрицы: ");
 int coord = Convert.ToInt32(Console.ReadLine());
 int[,,] matrix = new int[coord, coord, coord];
-inputMatrix(matrix);
+int[] array = new int[coord * coord * coord];
+inputMatrix(matrix, array);
+SwapFirstLastString(matrix, array);
 PrintMatrix(matrix);
 Console.WriteLine();
+Console.WriteLine($"Все числа: [{string.Join(", ", array)}]");
