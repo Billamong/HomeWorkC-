d9862bf0b448bf3a9a60bdf599b4556f76caf667
Denis <Dnik1993@yandex.ru>
2023-04-06 14:37:14 +0300

    Task 68

---
+++ Task 68/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int f(int a, int b)
+{
+    if (a == 0)
+        return b + 1;
+    else
+        if ((a != 0) && (b == 0))
+        return f(a - 1, 1);
+    else
+        return f(a - 1, f(a, b - 1));
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите последнее число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (m < 0 && m < 0)
+{
+    Console.WriteLine("Вы ошиблись");
+    Console.Write("Введите первое число: ");
+    n = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите последнее число: ");
+    m = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine(f(m, n));
