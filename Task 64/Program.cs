0c058778fb9d85bb5990135135cec5b9d13b9ad0
Denis <Dnik1993@yandex.ru>
2023-04-06 13:54:42 +0300

    Task 64

---
+++ Task 64/Program.cs
@@ -0,0 +1,22 @@
+﻿string f(int a, int b)
+{
+    if (a == b)
+        return $" {a}";
+    return $" {b}," + f(a, b - 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите последнее число: ");
+int m = int.Parse(Console.ReadLine()!);
+while (n > m)
+{
+    Console.WriteLine("Вы ошиблись");
+    Console.Write("Введите первое число: ");
+    n = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите последнее число: ");
+    m = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine(f(n, m));
