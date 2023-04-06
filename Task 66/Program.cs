1f077abc342674ebeeaa3ea2c7a7325deb27f146
Denis <Dnik1993@yandex.ru>
2023-04-06 14:10:42 +0300

    Task 66

---
+++ Task 66/Program.cs
@@ -0,0 +1,14 @@
+﻿int f(int a, int b)
+{
+    if (a ==b  )
+        return a;
+    return f(a, b - 1) + b;
+}
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите последнее число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
